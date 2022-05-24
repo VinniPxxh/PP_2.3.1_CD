4e6887e6e8432188fe9d10b73458204973d98885
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-24 09:30:30 +0300

    UserController created

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,63 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserService getUserService() {
+        return userService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "users")
+    public String listUsers(Model model) {
+        model.addAttribute("allUsers", userService.getAllUsers());
+        return "users";
+    }
+
+    @GetMapping(value = "new")
+    public String newUser(@ModelAttribute User user) {
+        return "new";
+    }
+
+    @PostMapping(value = "userAdd")
+    public String addUser(@ModelAttribute User user) {
+        userService.createUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String editUser(@PathVariable int id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "/edit";
+    }
+
+    @PostMapping(value = "/userUpdate")
+    public String updateUser(@ModelAttribute User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/remove/{id}")
+    public String removeUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+
+}
