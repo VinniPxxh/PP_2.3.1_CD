5ae800e1e3d8d696e5682755eae5eda6d24f8523
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-24 08:48:49 +0300

    configurers

--- src/main/java/web/config/AppConfig.java
+++ src/main/java/web/config/AppConfig.java
@@ -1,4 +1,5 @@
 package web.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
